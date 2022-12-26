93df9ad9f22e267e60dadefbefa6857950da9cf2
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-27 04:14:22 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,32 @@ import "../styles/App.css";
 import { movies } from "../utils/movieList";
 
 const App = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [results, setResult] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [results, setResult] = useState(null);
 
-    function handleClick(e) {}
+  function handleClick(e) {}
 
-    function handleChange(e) {}
+  function handleChange(e) {}
 
-    return (
-        <div id="main">
-            <form id="form">
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    value={inputValue}
-                    id="movie-input"
-                ></input>
-                <button id="button" onClick={handleClick}>
-                    Search
-                </button>
-            </form>
-            <div id="result">
-                {results &&
-                    results.map((movie) => (
-                        <div className="movie">{movie}</div>
-                    ))}
-            </div>
-        </div>
-    );
+  return (
+    <div id="main">
+      <form id="form">
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputValue}
+          id="movie-input"
+        ></input>
+        <button id="button" onClick={handleClick}>
+          Search
+        </button>
+      </form>
+      <div id="result">
+        {results &&
+          results.map((movie) => <div className="movie">{movie}</div>)}
+      </div>
+    </div>
+  );
 };
 
 export default App;
